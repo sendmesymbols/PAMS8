d089b448b58b6093decdee214ada71aebee715c2
Abdul <admin@gmail.com>
2026-04-07 10:49:06 +0500

    Restart

--- MS/Symbols/Screen.ts
+++ MS/Symbols/Screen.ts
@@ -65,7 +65,7 @@ export class Screen {
     this.view = view;
     this.isLine = isLine;
     this.layerManager = GraphicsLayerManager.getInstance(view);
-    this.symbolLayer = this.layerManager.getOrCreateLayer(LAYER_NAMES.FORCE);
+    this.symbolLayer = this.layerManager.getOrCreateLayer(LAYER_NAMES.TACT);
     this.amplifier = new Amplifier();
 
     // Initialize layers if not already done
